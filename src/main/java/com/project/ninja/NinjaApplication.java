4240445b5b4dfc8f90433d6c6e94558d04d6198e
Sadia Afrose <sadia.afrose@g.bracu.ac.bd>
2023-10-22 23:43:50 +0600

    initial commit

---
+++ src/main/java/com/project/ninja/NinjaApplication.java
@@ -0,0 +1,13 @@
+package com.project.ninja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaApplication.class, args);
+	}
+
+}
